d927214fa6ba2dcf5590334d2b8a5f8ab6949472
Avi E Koenig <darkmift@gmail.com>
2024-07-24 17:56:33 +0300

    Add project files.

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCDudu.Models;
+
+namespace MVCDudu.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCDudu.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "DUDU** Please enter your First name")]
+        public string Firstname { get; set; }
+    }
+}
